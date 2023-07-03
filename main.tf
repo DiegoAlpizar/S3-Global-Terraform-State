45e0c33c1e23203d0e61b02f87f07f2239162969
Diego Alpízar <48010731+DiegoAlpizar@users.noreply.github.com>
2023-07-03 12:55:34 -0600

    Initial commit
    Already deployed S3 Terraform backend

---
+++ main.tf
@@ -0,0 +1,92 @@
+terraform {
+
+  backend "s3" {
+    
+    bucket = "global-terraform-state-demo"
+    key = "global/terraform.tfstate"
+    encrypt = true
+
+    dynamodb_table = "terraform_state_lock"
+
+    region = "us-east-1"
+
+  }
+
+  required_providers {
+
+    aws = {
+
+      source = "hashicorp/aws"
+      version = "5.6.2"
+
+    }
+
+  }
+  
+}
+
+
+provider "aws" {
+
+  region = "us-east-1"
+  
+}
+
+
+resource "aws_s3_bucket" "terraform_state" {
+
+  bucket = "global-terraform-state-demo"
+
+  # Prevent accidental deletion of this S3 bucket
+  lifecycle {
+    prevent_destroy = true
+  }
+
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state" {
+
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration { status = "Enabled" }
+  
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+
+}
+
+resource "aws_s3_bucket_public_access_block" "terraform_state" {
+  
+  bucket = aws_s3_bucket.terraform_state.id
+
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+
+}
+
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  
+  name = "terraform_state_lock"
+
+  billing_mode = "PROVISIONED"
+  read_capacity = 1
+  write_capacity = 1
+  
+  hash_key = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+}
